8694fded4e5529dd7c62c3848d0afad425f7b039
AVasilaki <poseydon463@gmail.com>
2023-08-10 19:37:06 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const container = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function createMurcup(arr) {
+  console.log(arr);
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}"  alt="${description}" />
+      </a>
+    </li>`
+    )
+    .join('');
+}
+container.insertAdjacentHTML('beforeend', createMurcup(galleryItems));
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
