b8271fb36674b0ddc26ed45f8458589dd045583b
AVasilaki <poseydon463@gmail.com>
2023-08-12 11:24:33 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+const time = localStorage.getItem('videoplayer-current-time');
+console.log(time);
+iframePlayer.setCurrentTime(time);
+iframePlayer.on('timeupdate', throttle(onPlay, 1000));
